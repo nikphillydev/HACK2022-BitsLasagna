24cb00309e55edad9a48966686f997173391cf00
Matthew Lai <matthewlai@sahw.ca>
2022-11-05 16:47:21 -0600

    functionality

--- hackathon_arduino.ino
+++ hackathon_arduino.ino
@@ -1,6 +1,7 @@
 #include <AFMotor.h>
 
 AF_DCMotor leftMotor(1);
+AF_DCMotor rightMotor();
 
 /*
 motor.run(FORWARD/BACKWARD/RELEASE)
@@ -14,21 +15,48 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  leftMotor.run(RELEASE);
+  forward();
+  backward();
+  left();
+  right();
 }
 
 void forward() {
-
+leftMotor.setSpeed(255);
+rightMotor.setSpeed(255);
+rightMotor.run(FORWARD);
+leftMotor.run(FORWARD);
+delay(3000);
+leftMotor.run(RELEASE);
+rightMotor.run(RELEASE);
 }
 
 void backward() {
-
+leftMotor.setSpeed(255);
+rightMotor.setSpeed(255);
+rightMotor.run(BACKWARD);
+leftMotor.run(BACKWARD);
+delay(3000);
+leftMotor.run(RELEASE);
+rightMotor.run(RELEASE);
 }
 
 void turnRight() {
-
+leftMotor.setSpeed(255);
+rightMotor.setSpeed(100);
+rightMotor.run(FORWARD);
+leftMotor.run(FORWARD);
+delay(3000);
+leftMotor.run(RELEASE);
+rightMotor.run(RELEASE);
 }
 
 void turnLeft() {
-  
+leftMotor.setSpeed(100);
+rightMotor.setSpeed(255);
+rightMotor.run(FORWARD);
+leftMotor.run(FORWARD);
+delay(3000);
+leftMotor.run(RELEASE);
+rightMotor.run(RELEASE);
 }
