85f51b2970a690d3f838239a9e50fbab5f1d8f1d
Nikolai Philipenko <nikol@LAPTOP-6GDJ0M02.localdomain>
2022-11-05 19:22:49 -0600

    update

--- bluetooth.py
+++ bluetooth.py
@@ -6,6 +6,16 @@ port="COM4" #This will be different for various devices and on windows it will p
 bluetooth=serial.Serial(port, 9600)#Start communications with the bluetooth unit
 print("Connected")
 bluetooth.flushInput() #This gives the bluetooth a little kick
-bluetooth.write(b'1')
+control = input()
+while control != 'q':
+	if control == 'w':
+		bluetooth.write(b'w')
+	if control == 'a':
+		bluetooth.write(b'a')
+	if control == 's':
+		bluetooth.write(b's')
+	if control == 'd':
+		bluetooth.write(b'd')
+	control = input()
 bluetooth.close() #Otherwise the connection will remain open until a timeout which ties up the /dev/thingamabob
 print("Done")
